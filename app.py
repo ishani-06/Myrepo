acc81a63e15deea0724cbd6d5ec897de00d857b2
ishani-06 <pandaishani06@gmail.com>
2026-05-24 10:50:56 +0530

    Updated Flask App

--- app.py
+++ app.py
@@ -1,32 +1,14 @@
 from flask import Flask, render_template
-import os
 
 app = Flask(__name__)
 
-# Create count.txt if it doesn't exist
-if not os.path.exists("count.txt"):
-    with open("count.txt", "w") as f:
-        f.write("0")
-
 @app.route('/')
-def hello():
+def home():
     return render_template('index.html')
 
 @app.route('/hello/<name>')
-def hello_name(name):
-
-    # Read count
-    with open("count.txt", "r") as f:
-        count = int(f.read())
-
-    # Increment count
-    count += 1
-
-    # Save updated count
-    with open("count.txt", "w") as f:
-        f.write(str(count))
-
-    return render_template('hello.html', name=name, count=count)
+def hello(name):
+    return render_template('hello.html', name=name)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=False)
